005a4cbd3b134224b26b483720e0da014db4672f
kryston69 <krystonqxxx@gmail.com>
2022-06-23 13:37:37 +0200

    added importent comment

--- src/main/java/net/kryston/tutorialmod/TutorialMod.java
+++ src/main/java/net/kryston/tutorialmod/TutorialMod.java
@@ -9,7 +9,7 @@ public class TutorialMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 
-	//important change
+	//very important commnent
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
